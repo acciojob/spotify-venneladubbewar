92924515a4e2090642399b99a753a99b1cbdb16b
kiran <Kiran.dubbewar18@users.noreply.github.com>
2023-03-05 22:06:12 +0530

    ven 1.2

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -7,30 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.sql.SQLOutput;
 
 @SpringBootApplication
-//public class Application {
- public class Test {
-	//public static void main(String[] args) {
+public class Application {
 
-	//	SpringApplication.run(Application.class, args);
-		public Test()
-		{
-			System.out.println("1");
-			new Test(10);
-			System.out.println("5");
-		}
-		public Test(int temp)
-		{
-			System.out.println("2");
-			new Test(10, 20);
-			System.out.println("4");
-		}
-		public Test(int data, int temp)
-		{
-			System.out.println("3");
-		}
-		public static void main(String[] args) {
-			Test obj = new Test();
-		}
+	public static void main(String[] args) {
 
-	//}
+		SpringApplication.run(Application.class, args);
+
+	}
 }
